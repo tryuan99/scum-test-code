05862f5774df7c6108936b751f2b1de1b49097ee
Titan Yuan <tryuan99@gmail.com>
2024-07-22 19:11:44 -0700

    Average over multiple ADC outputs for potentiometric sensors

--- scm_v3c/applications/sensor_adc/sensor_potentiometric.c
+++ scm_v3c/applications/sensor_adc/sensor_potentiometric.c
@@ -1,7 +1,18 @@
 #include "sensor_potentiometric.h"
 
 #include <stdint.h>
+#include <stdlib.h>
 
 #include "adc.h"
 
-uint16_t sensor_potentiometric_measure(void) { return adc_read_output(); }
+// Number of samples to average over.
+#define NUM_SAMPLES_TO_AVERAGE 64
+
+uint16_t sensor_potentiometric_measure(void) {
+    // Average over multiple ADC outputs.
+    uint16_t adc_output_sum = 0;
+    for (size_t i = 0; i < NUM_SAMPLES_TO_AVERAGE; ++i) {
+        adc_output_sum += adc_read_output();
+    }
+    return adc_output_sum / 64;
+}
